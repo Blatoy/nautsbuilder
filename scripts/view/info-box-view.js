284613e6965cb9eaabdc009a051e43f7d049996c
Blatoy <lock.jeanneret@gmail.com>
2017-09-10 13:22:55 +0200

    Removes debug info

--- scripts/view/info-box-view.js
+++ scripts/view/info-box-view.js
@@ -70,7 +70,7 @@ var InfoBoxView = new function() {
       var content = "";
 
       for(var k in effects) {
-        content += Effect.toString({unit: effects[k].unit, value: effects[k].value, key: k}) + effects[k].scaleType + " <br>";
+        content += Effect.toString({unit: effects[k].unit, value: effects[k].value, key: k}) + "<br>";
       }
 
       content += "<span class='build-summary-cost'>" + Build.current.getRowPrice(i) + "<img src='" + CONFIG.path.images + "solar-icon.png'></span>"
