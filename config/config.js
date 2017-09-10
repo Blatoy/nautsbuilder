cbca4e08d8ec84c6e448c25c3026636887131ebb
Blatoy <lock.jeanneret@gmail.com>
2017-09-10 02:01:59 +0200

    Remove unused files, fix config and add visitor counter in index

--- config/config.js
+++ config/config.js
@@ -1,7 +1,6 @@
 // If you want to change the path, you will also have to change:
 // - index.php, src to main.js and config.js
 // - All the css: urls to images
-<<<<<<< HEAD
 var CONFIG = new function() {
   this.scriptList = [
     "libs/jquery.min.js",
@@ -24,7 +23,7 @@ var CONFIG = new function() {
     "view/shop-view.js",
     "view/message-box-view.js"
   ];
-  // DPS: 840
+
   this.apiURL = "https://orikaru.net/resources/logic/php/ajax/awesomenauts.php";
 
   this.dpsCalculationRegex = {
@@ -38,38 +37,6 @@ var CONFIG = new function() {
   path.base = "/nautsbuilder/";
   path.scripts = path.base + "scripts/";
   path.images = path.base + "images/";
-=======
-var CONFIG = new function(){
-  this.scriptList = [
-    "jquery.min.js",
-    "event-manager.js",
-    "nautsbuilder.js",
-    "skill-parser.js",
-    "tooltip.js",
-    "ui/manager.js",
-    "ui/shop.js",
-    "ui/summary.js",
-    "ui/upgrade.js",
-    "ui/naut-list.js",
-    "utils.js"
-  ];
-
-  this.styleList = [
-    "main.css",
-    "naut-info.css",
-    "shop.css",
-    "tooltip.css",
-    "upgrade.css"
-  ];
-
-  this.apiURL = "https://orikaru.net/resources/logic/php/ajax/awesomenauts.php";
-
-  var path = {};
-  path["base"] = "/nautsbuilder/";
-  path["scripts"] = path["base"] + "scripts/";
-  path["images"] = path["base"] + "images/";
-  path["styles"] = path["base"] + "styles/";
->>>>>>> master
 
   this.path = path;
 };
